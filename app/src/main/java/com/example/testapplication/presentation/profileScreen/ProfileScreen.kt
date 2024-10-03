7d05027c21a0d60eea06dbd5f0cbca6fbdbf3f2d
Temza1 <artem.zateev.98@mail.ru>
2024-10-03 12:56:04 +0300

    перед доработкой экрана чата и профиля

--- app/src/main/java/com/example/testapplication/presentation/profileScreen/ProfileScreen.kt
+++ app/src/main/java/com/example/testapplication/presentation/profileScreen/ProfileScreen.kt
@@ -24,103 +24,103 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.testapplication.domain.model.chats.User
 
-@Composable
-fun ProfileScreen(
-    onClickReturnButton: () -> Unit
-) {
-    ProfileScreenContent(modifier = Modifier, onClickReturnButton)
-}
-@Composable
-fun ProfileScreenContent(
-    modifier: Modifier = Modifier,
-    onClickReturnButton: () -> Unit
-) {
-    Column {
-        Box(
-            modifier = modifier
-                .fillMaxWidth()
-                .background(color = Color.LightGray)
-                .size(0.dp, 250.dp)
-        ) {
-
-            IconButton(onClick = { onClickReturnButton() }, modifier.padding(10.dp,22.dp)) {
-                Icon(
-                    Icons.AutoMirrored.Filled.ArrowBack,
-                    contentDescription = "content description"
-                )
-            }
-
-
-        }
-        Box(
-            modifier = modifier
-                .background(color = Color.White)
-                .fillMaxSize()
-        ) {
-            InfoAboutUser(modifier, user)
-        }
-    }
-}
-
-@Composable
-fun InfoAboutUser(
-    modifier: Modifier,
-    worker: User
-) {
-    Column {
-        Row(
-            modifier = modifier.padding(20.dp, 20.dp)
-        ) {
-
-            Icon(
-                Icons.Filled.Star,
-                modifier = Modifier.padding(
-                    0.dp,0.dp,10.dp,0.dp,
-                ),
-                contentDescription = "content description"
-            )
-
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    worker.birthday,
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Medium
-                )
-                Text(
-                    worker.birthday,
-                    fontSize = 18.sp,
-                    color = Color.Gray,
-                    fontWeight = FontWeight.Medium
-                )
-            }
-
-        }
-
-        Row(
-            modifier = modifier.padding(20.dp, 0.dp)
-        ) {
-
-            Icon(
-                Icons.Filled.Call,
-                modifier = Modifier.padding(
-                    0.dp,0.dp,10.dp,0.dp,
-                ),
-                contentDescription = "content description"
-            )
-
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(
-                    worker.phone,
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Medium
-                )
-            }
-        }
-    }
-}
+//@Composable
+//fun ProfileScreen(
+//    onClickReturnButton: () -> Unit
+//) {
+//    ProfileScreenContent(modifier = Modifier, onClickReturnButton)
+//}
+//@Composable
+//fun ProfileScreenContent(
+//    modifier: Modifier = Modifier,
+//    onClickReturnButton: () -> Unit
+//) {
+//    Column {
+//        Box(
+//            modifier = modifier
+//                .fillMaxWidth()
+//                .background(color = Color.LightGray)
+//                .size(0.dp, 250.dp)
+//        ) {
+//
+//            IconButton(onClick = { onClickReturnButton() }, modifier.padding(10.dp,22.dp)) {
+//                Icon(
+//                    Icons.AutoMirrored.Filled.ArrowBack,
+//                    contentDescription = "content description"
+//                )
+//            }
+//
+//
+//        }
+//        Box(
+//            modifier = modifier
+//                .background(color = Color.White)
+//                .fillMaxSize()
+//        ) {
+//            InfoAboutUser(modifier, user)
+//        }
+//    }
+//}
+//
+//@Composable
+//fun InfoAboutUser(
+//    modifier: Modifier,
+//    worker: User
+//) {
+//    Column {
+//        Row(
+//            modifier = modifier.padding(20.dp, 20.dp)
+//        ) {
+//
+//            Icon(
+//                Icons.Filled.Star,
+//                modifier = Modifier.padding(
+//                    0.dp,0.dp,10.dp,0.dp,
+//                ),
+//                contentDescription = "content description"
+//            )
+//
+//
+//            Row(
+//                modifier = Modifier.fillMaxWidth(),
+//                horizontalArrangement = Arrangement.SpaceBetween
+//            ) {
+//                Text(
+//                    worker.birthday,
+//                    fontSize = 18.sp,
+//                    fontWeight = FontWeight.Medium
+//                )
+//                Text(
+//                    worker.birthday,
+//                    fontSize = 18.sp,
+//                    color = Color.Gray,
+//                    fontWeight = FontWeight.Medium
+//                )
+//            }
+//
+//        }
+//
+//        Row(
+//            modifier = modifier.padding(20.dp, 0.dp)
+//        ) {
+//
+//            Icon(
+//                Icons.Filled.Call,
+//                modifier = Modifier.padding(
+//                    0.dp,0.dp,10.dp,0.dp,
+//                ),
+//                contentDescription = "content description"
+//            )
+//
+//            Row(
+//                modifier = Modifier.fillMaxWidth()
+//            ) {
+//                Text(
+//                    worker.phone,
+//                    fontSize = 18.sp,
+//                    fontWeight = FontWeight.Medium
+//                )
+//            }
+//        }
+//    }
+//}
